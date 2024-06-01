d5fc84f1d38f4f7af4d5b39e6def475373822065
Koraze <khoraze@gmail.com>
2024-06-01 10:08:11 -0400

    Update main.py

--- carte/main.py
+++ carte/main.py
@@ -1,10 +1,6 @@
-
-
-```python
 import machine
 
 exec(open("./exemple/reset_neopixel.py").read())
 if cause == machine.PWRON_RESET or cause == machine.HARD_RESET or cause == machine.DEEPSLEEP_RESET :
     print("launching main")
     exec(open("./exemple/mqtt_ha.py").read())
-```
